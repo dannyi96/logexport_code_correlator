f6ce02c3695e5555c5610792bb864a803ed598a7
dannyi96 <danielbcbs2@gmail.com>
2022-08-20 16:36:40 +0530

    init

--- logexport_code_correlator/log_extractor.py
+++ logexport_code_correlator/log_extractor.py
@@ -3,28 +3,44 @@ import re
 import os
 import csv 
 
-fields = ['file', 'query', 'events', 'bytes']
-rows = []
-elems = sys.stdin.readlines()
-for elem in elems:
-    line_elems = elem.split()
-    file = line_elems[0].replace(':','')
-    query_filters = []
-    for string in re.findall('"([^"]*)"', elem):
-        if string != '':
-            query_filters.append('"%s"'%(string))
-    if(len(query_filters) > 0):
-        query = ' AND '.join(query_filters)#.replace(',','<comma>').replace('"','\"')
-        print(query)
-        rows.append([file, query, -1, -1])
-
-filename = "log_stats_template.csv"
+class BaseLogLineExtractor:
+    def __init__(self):
+        pass
     
-# writing to csv file 
-with open(filename, 'w') as csvfile: 
-    # creating a csv writer object 
-    csvwriter = csv.writer(csvfile, delimiter='\t') 
-    # writing the fields 
-    csvwriter.writerow(fields)
-    # writing the data rows 
-    csvwriter.writerows(rows)
+    def extract_logs(self, codebase_folder: str, delimiters: list) -> None:
+        pass
+    
+class CLogLineExtractor(BaseLogLineExtractor):
+    def __init__():
+        pass
+    
+    def extract_logs(self, codebase_folder: str, delimiters: list) -> None:
+        
+        
+    def to_csv(self, filename: str = "log_stats_template.csv") -> None:
+        # grep -r -E 'MPSLogger::ins|MPS_DEBUG_LOG|MPS_INFORMATION_LOG|MPS_CRITICAL_LOG|MPS_FATAL_LOG
+        # |MPS_ERROR_LOG|MPS_WARNING_LOG|MPS_NOTICE_LOG|MPS_TRACE_LOG' * | python extract_stings.py
+
+        fields = ['file', 'log_line', 'query', 'events', 'bytes']
+        rows = []
+        elems = sys.stdin.readlines()
+        for elem in elems:
+            line_elems = elem.split()
+            file = line_elems[0].replace(':','')
+            query_filters = []
+            for string in re.findall('"([^"]*)"', elem):
+                if string != '':
+                    query_filters.append('"%s"'%(string))
+            if(len(query_filters) > 0):
+                query = ' AND '.join(query_filters)#.replace(',','<comma>').replace('"','\"')
+                print(query)
+                rows.append([file, file, query, -1, -1])
+
+        # writing to csv file 
+        with open(filename, 'w') as csvfile: 
+            # creating a csv writer object 
+            csvwriter = csv.writer(csvfile, delimiter='\t') 
+            # writing the fields 
+            csvwriter.writerow(fields)
+            # writing the data rows 
+            csvwriter.writerows(rows)
